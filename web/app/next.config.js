7441e7675b80ca97ce7bad1b6bc9bb19eb20ad02
Geoff Johnson <geoff.jay@gmail.com>
2022-11-15 21:57:31 -0800

    feat: setup tailwindcss

--- web/app/next.config.js
+++ web/app/next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack: (config) => {
+    config.plugins = config.plugins || [];
 
-module.exports = nextConfig
+    config.optimization.providedExports = true;
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "./"),
+      "@styles": path.resolve(__dirname, "./styles"),
+    };
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
